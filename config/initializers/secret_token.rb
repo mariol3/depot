b3c030da63c0629875218d2cf9a34a313c766255
Mario Grimaldi <mario.grimaldi89@gmail.com>
2013-11-17 21:13:41 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '49f99adf3bac6b446118547f99a3ed19fc1be1afd59c5ca9b1d5756056b4d05df2c3806e9f4c1e79508025b8f26c8510fa03b465f3122054e0e161e5d4a851ce'
---
+++ db/seeds.rb
@@ -0,0 +1,38 @@
+Product.delete_all
+
+Product.create(title: 'Libro fico',
+  description:
+    %{<p>
+        Questo libro e' veramente fico. Da leggere assolutamente. No dico davvero...
+      </p>},
+  image_url: 'http://www.macrolibrarsi.it/data/cop/zoom/i/il-fico-fiorisce-libro-60271.jpg',
+  price: 29.99
+)
+
+Product.create(title: 'D&D for dummies',
+  description:
+    %{<p>
+        Il manuale per iniziare con D&D. Tutto quello che serve ad un niubbo che vuole diventare
+        un vero pro ;)
+      </p>},
+  image_url: 'http://criptadelconte.files.wordpress.com/2011/01/dd1bf1.png',
+  price: 29.99
+)
+
+Product.create(title: 'Agile Web Development with Ruby on Rails',
+  description:
+    %{<p>
+        Il libro da cui parte la creazione di questa applicazione in Ruby on Rails.
+      </p>},
+  image_url: 'http://ecx.images-amazon.com/images/I/51Pkt8UcdAL.jpg',
+  price: 29.99
+)
+
+Product.create(title: 'Il signore degli anelli',
+  description:
+    %{<p>
+        Il mio tessssoro....
+      </p>},
+  image_url: 'http://www.paolomarzola.com/blog/wp-content/uploads/2009/01/il_signore_degli_anelli.jpg',
+  price: 20.00
+)
