4120b633f6c5dd7ec8cc46fe8f6736e7ec8d1927
Mario Grimaldi <mario.grimaldi89@gmail.com>
2013-11-18 19:29:24 +0100

    Added email send when order is placed.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "mario.grimaldi89",
+    password: "m3a3t3r3i3xgmail",
+    enable_starttls_auto: true
+  }
+end
