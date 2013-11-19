6233f7926ed94f5b4701ad1a7f07ae8d3ece1641
Mario Grimaldi <m.grimaldi@addiction.it>
2013-11-19 15:34:06 +0100

    Setup for other locales.

--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,21 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
   
-  resources :users
-
-  resources :orders
-
-  resources :line_items do
-    member do
-      post 'decrement'
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items do
+      member do
+        post 'decrement'
+      end
     end
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root :to => 'store#index', as: 'store'
   end
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-    get :who_bought, on: :member
-  end
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -75,7 +73,6 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', as: 'store'
 
   # See how all your routes lay out with "rake routes"
 
